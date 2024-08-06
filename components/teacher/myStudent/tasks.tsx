eed6bd3321c0031ab25fc2afe0185501fcd298fc
AvivHagag <Aviv1049@gmail.com>
2024-08-06 21:15:36 +0300

    BSPMS243-42

--- components/teacher/myStudent/tasks.tsx
+++ components/teacher/myStudent/tasks.tsx
@@ -1,4 +1,13 @@
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { TeacherTask } from "@prisma/client";
 import Link from "next/link";
 
@@ -21,6 +30,51 @@ export async function Tasks({ id, tasks, name }: TasksProps) {
           </Button>
         </Link>
       </div>
+      {tasks && (
+        <div className="flex flex-col justify-center w-2/3 mx-auto bg-white bg-opacity-20 border border-white border-opacity-30 rounded-lg shadow-xl backdrop-blur">
+          <Table dir="rtl">
+            <TableCaption>המטלות האחרונות של {name}</TableCaption>
+            <TableHeader dir="rtl">
+              <TableRow dir="rtl">
+                <TableHead className="text-right font-medium text-darkRed">
+                  מספר
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  שם המטלה
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  להגשה עד
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  הודעה
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  ציון
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  סטטוס
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody dir="rtl">
+              {tasks.map((task, index) => (
+                <TableRow key={task.id}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell className="font-medium">{task.id}</TableCell>
+                  <TableCell>
+                    {new Date(task.date).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>{task.messageText}</TableCell>
+                  <TableCell>{task.grade ?? " - "}</TableCell>
+                  <TableCell>
+                    {task.approvedByAdmin ? "מאושר" : "ממתין לאישור מנהל האתר"}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      )}
     </>
   );
 }
