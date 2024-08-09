61553c25ea8e59fbff045a4a63048f18ba86aac2
Daniel-Arvili <danarvili@gmail.com>
2024-08-09 11:51:28 +0300

    BSPM243-155

--- app/teacher/createtask/page.tsx
+++ app/teacher/createtask/page.tsx
@@ -1,11 +1,13 @@
+import TitleAndButton from "@/components/headerNav/TitleAndButton";
 import { TaskCreator } from "@/components/teacher/task/taskCreator";
+import { Button } from "@/components/ui/button";
 import { getStudentById } from "@/data/user";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default async function CreateTask({
   searchParams,
-}: {
-  searchParams: { [key: string]: string };
-}) {
+@@ -9,12 +13,9 @@ export default async function CreateTask({
   const student = await getStudentById(searchParams.id);
   return (
     <>
@@ -15,6 +17,9 @@ export default async function CreateTask({
       >
         יצירת משימה לתלמיד - {student?.name}
       </h1>
+      <div className="mx-2 lg:mx-8 2xl:mx-16">
+        <TitleAndButton PageName={`יצירת משימה לתלמיד - ${student?.name}`} />
+      </div>
       <div className="flex justify-center min-h-screen md:min-h-[500px] mx-2 lg:mx-8 2xl:mx-16 border border-mediumBeige shadow-xl rounded-lg bg-lightBeige">
         {student && <TaskCreator student={student} />}
       </div>
